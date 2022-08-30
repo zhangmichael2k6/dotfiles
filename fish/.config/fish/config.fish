7c5aff1f14a7371b9458f5e905c6c357b20f212b
zhangmichael2k6 <zhangmichael2006@proton.me>
2022-08-30 16:58:05 +0700

    added and fixed fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-starship init fish | source
+#starship init fish | source
 
 if type -q exa
   alias ll "exa -l -g --icons"
@@ -6,3 +6,8 @@ if type -q exa
 end
 
 alias vim="nvim"
+alias cls="clear"
+
+eval "$(oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/emodipt-extend.omp.json)"
+
+set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
