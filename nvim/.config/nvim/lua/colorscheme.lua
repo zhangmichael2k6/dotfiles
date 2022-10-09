b698b98d8f81492aff8f81fae12bc26cae8123c4
zhangmichael2k6 <zhangmichael2006@proton.com>
2022-10-09 13:55:49 +0700

    changed colourscheme

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,5 +1,8 @@
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+-- vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+-- require("catppuccin").setup()
+-- vim.cmd [[colorscheme catppuccin]]
 
-require("catppuccin").setup()
-
-vim.cmd [[colorscheme catppuccin]]
+require('onedark').setup {
+    style = 'deep'
+}
+require('onedark').load()
