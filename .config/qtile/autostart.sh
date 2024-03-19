7b35ed828b490f8b5d982918fdd40f60e2d012f5
zhangmichael2k6 <zhangmichael2006@proton.me>
2024-03-19 17:35:59 +0700

    rbvuwebrvubev

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,7 +2,7 @@
 picom &
 nm-applet --indicator &
 fcitx &
-nitrogen --random --set-zoom-fill ~/.wallpaper&
+variety &
 flameshot &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 sxhkd -c $HOME/.config/sxhkd/sxhkdrc &
